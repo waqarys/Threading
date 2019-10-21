15a6640346d8aeb86c0fd37fc64208e3d25dbd76
waqar <waqaryshaikh@gmail.com>
2019-10-21 10:59:24 +0530

    Matrix Multiplication

---
+++ Threads/src/com/waqar/concurrency/matrix/multiplication/main/ParallelIndividualMain.java
@@ -0,0 +1,25 @@
+package com.waqar.concurrency.matrix.multiplication.main;
+
+import com.waqar.concurrency.matrix.multiplication.parallel.individual.ParallelIndividualMultiplier;
+import com.waqar.concurrency.matrix.multiplication.util.MatrixGenerator;
+
+import java.util.Date;
+
+public class ParallelIndividualMain {
+    public static void main(String[] args) {
+        double matrix1[][] = MatrixGenerator.generate(2000, 2000);
+        double matrix2[][] = MatrixGenerator.generate(2000, 2000);
+        double resultParallel[][]= new double[matrix1.length][matrix2[0].length];
+
+        Date start, end;
+
+        System.out.println("Start");
+
+        start=new Date();
+        ParallelIndividualMultiplier.multiply(matrix1, matrix2, resultParallel);
+        end=new Date();
+        System.out.printf("Parallel Individual: %d%n",end.getTime()-start.getTime());
+
+    }
+
+}
